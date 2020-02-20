feb91ceaca674c4c4ed03c6b8aeeb65a2e63aded
ritchie46 <ritchie46@gmail.com>
2020-02-20 09:41:49 +0100

    reduce image sizes

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,9 @@
+# first compress and resize images (resizing is only upper bound per dimension)
+mogrify -resize 800x600 -format jpg -quality 70 static/img/**/*.{jpg,png}
+
 hugo && rsync -v -r public/. linode:/var/www/blog
+
+# remove the generated jpg files and only keep originals.
+git clean -f static/img/
+git checkout -- static/img/
+
