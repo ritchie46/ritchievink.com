6cc0ee27aa3f94c697abb0d289eef02a49506560
ritchie46 <ritchie46@gmail.com>
2020-03-04 14:07:13 +0100

    resize large png to a maximum of 800px and apply maximum png compression

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,18 @@
-# first compress and resize images (resizing is only upper bound per dimension)
-mogrify -resize 800x600 -format jpg -quality 70 static/img/**/*.{jpg,png}
+# first compress and resize images
+# only images wider than 800px are resized.
+echo compress jpegs
+mogrify -resize '800>' -format jpg -quality 85 static/img/**/*.{jpg,png}
 
-hugo && rsync -v -r public/. linode:/var/www/blog
+echo compress pngs
+# png compression is lossless so we go for maximum compression
+for f in static/img/**/*.png; do 
+	convert -resize '800>' -format png -quality 1 $f $f
+done
 
+echo build site and rsync
+hugo && rsync --exclude='*.swp' -v -r public/. linode:/var/www/blog
+
+echo delete generated images
 # remove the generated jpg files and only keep originals.
 git clean -f static/img/
 git checkout -- static/img/
